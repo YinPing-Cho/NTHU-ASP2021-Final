a07dc11e7149ef4b36b1b8ea1b69fbc73ca1e1dc
catel3 <96810069+catel3@users.noreply.github.com>
2022-01-01 16:56:51 +0800

    PARAMS_NLMS_DFE.m

--- PARAMS_NLMS_DFE.m
+++ PARAMS_NLMS_DFE.m
@@ -1,10 +1,20 @@
 function Params = PARAMS_NLMS_DFE(Params)
     % For Static case
-
+    Params.Static.NLMS_DFE.Lfff = 20;
+    Params.Static.NLMS_DFE.Lfbf = 20;
+    Params.Static.NLMS_DFE.alpha = 0.05; 
+    Params.Static.NLMS_DFE.c = 0.001;
     
     % For Q_Static case
-
+    Params.Q_Static.NLMS_DFE.Lfff = 15;
+    Params.Q_Static.NLMS_DFE.Lfbf = 10;
+    Params.Q_Static.NLMS_DFE.alpha = 0.05; 
+    Params.Q_Static.NLMS_DFE.c = 0.001;
     
     % For T_Varying case
+    Params.T_Varying.NLMS_DFE.Lfff = 15;
+    Params.T_Varying.NLMS_DFE.Lfbf = 10;
+    Params.T_Varying.NLMS_DFE.alpha = 0.05; 
+    Params.T_Varying.NLMS_DFE.c = 0.001;
 
 end
