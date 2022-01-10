265d38fea0268b2d6073a97f6fae021552179445
Yin-Ping Cho <63432757+YinPing-Cho@users.noreply.github.com>
2022-01-10 17:24:57 +0800

    update RLS-DFE params and validate demo data

--- validate_output_mat.m
+++ validate_output_mat.m
@@ -2,3 +2,16 @@ clear;
 load ans_static.mat;
 load ans_qstatic.mat;
 load ans_varying.mat;
+
+disp(calc_binary_ratio(ans_static_1))
+disp(calc_binary_ratio(ans_static_2))
+
+disp(calc_binary_ratio(ans_qstatic_1))
+disp(calc_binary_ratio(ans_qstatic_2))
+
+disp(calc_binary_ratio(ans_varying_1))
+disp(calc_binary_ratio(ans_varying_2))
+
+function ratio = calc_binary_ratio(seq)
+    ratio = sum(seq(:) == 1) / sum(seq(:) == -1);
+end
