53ea783103794fae288cb8ed68675a9bc3b41b04
Yin-Ping Cho <63432757+YinPing-Cho@users.noreply.github.com>
2022-01-10 17:48:29 +0800

    use final RLS-DFE params

--- PARAMS_RLS_DFE.m
+++ PARAMS_RLS_DFE.m
@@ -2,13 +2,13 @@ function Params = PARAMS_RLS_DFE(Params)
     % For Static case
     Params.Static.RLS_DFE.Lfff = 10;
     Params.Static.RLS_DFE.Lfbf = 10;
-    Params.Static.RLS_DFE.lambda = 0.998; 
+    Params.Static.RLS_DFE.lambda = 0.997; 
     Params.Static.RLS_DFE.delta = 1;
     
     % For Q_Static case
     Params.Q_Static.RLS_DFE.Lfff = 10;
-    Params.Q_Static.RLS_DFE.Lfbf = 9;
-    Params.Q_Static.RLS_DFE.lambda = 0.997; 
+    Params.Q_Static.RLS_DFE.Lfbf = 10;
+    Params.Q_Static.RLS_DFE.lambda = 0.9992; 
     Params.Q_Static.RLS_DFE.delta = 1;
     
     % For T_Varying case
